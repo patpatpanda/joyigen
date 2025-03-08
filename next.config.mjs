c3c29a205fbdafd8f59df435530d68f701471e14
patpatpanda <nils-emil1337@hotmail.se>
2025-03-08 13:45:05 +0100

    Ditt meddelande

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone", // Gör builden mindre och optimerad
 
-// next.config.mjs
-export default {
+  experimental: {
+    optimizeCss: true, // Minskar storleken på CSS-filer
+    turboMode: true, // Förbättrar build-hastigheten
+  },
+
+  webpack: (config) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+      maxSize: 2000000, // Dela upp stora filer i mindre bitar (2MB)
+    };
+    return config;
+  },
+
   images: {
-    domains: ['joybilder.blob.core.windows.net'], // Lägg till din Blob Storage domän här
+    domains: ["joybilder.blob.core.windows.net"], // Din Blob Storage domän
   },
 };
 
+export default nextConfig;
